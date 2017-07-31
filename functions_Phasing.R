35236f0c35fa32f5c5b3c269a0d9819009e67ca4
Isabel Alves <ialves@ad.oicr.on.ca>
2017-07-31 15:49:20 -0400

    Merges the whole HQ hap into a new, per-site LQhap

--- functions_Phasing.R
+++ functions_Phasing.R
@@ -34,7 +34,7 @@ excludingDuplicates <- function(tmpUniqueHapList) {
   countDupList <- 1
   
   
-  while(T) { #merging haplopytes along the list
+while(T) { # haplopytes along the list
     
     tmp_hapList <- mergingHaplotypes(tmpUniqueHapList)
     tmp_namesHapList <- namesHapListFunction(tmp_hapList)
@@ -274,7 +274,7 @@ mergingHaplotypes <- function(hapList) {
   
 newHapList_tmp <- hapList
   #newHapList_tmp <- newHap
-  #newHapList_tmp <- sortedHapList
+  #newHapList_tmp <- listFakeHaps
   newHapList_def <- list()
   
   
@@ -284,9 +284,12 @@ newHapList_tmp <- hapList
   
   while(indexList_j <= length(newHapList_tmp)) {
     
-    z <- is.element(colnames(newHapList_tmp[[indexList_i]]), colnames(newHapList_tmp[[indexList_j]]))    
+    z <- is.element(colnames(newHapList_tmp[[indexList_i]]), colnames(newHapList_tmp[[indexList_j]])) 
+    #print(sum(z))
     if (sum(z) == 0) { #means that uniqueHap[[indexList]] is not contained in the next haplotype
       #this is in the case the first and nd are non-overlapping
+      #option A
+      #print("option A")
       print(paste("Haplotype:", indexList_i, "is not contained within the haplotype:", indexList_j, sep=" "))
       newHapList_tmp[[indexList_i]] <- newHapList_tmp[[indexList_i]]
       newHapList_def[[countHaps]] <- newHapList_tmp[[indexList_i]]
@@ -305,21 +308,21 @@ newHapList_tmp <- hapList
     } else { #the hap is contained in the hap+1
       
       if (sum(z) == length(colnames(newHapList_tmp[[indexList_i]]))) { #the hap is fully contained in hap+1
-        
+        #print("option B")
         newHapList_tmp[[indexList_i]] <- newHapList_tmp[[indexList_j]]
         
       } else if (sum(z) == length(colnames(newHapList_tmp[[indexList_j]]))) {  #the hap+1 is fully contained in hap
-        
+        #print("option C")
         newHapList_tmp[[indexList_i]] <- newHapList_tmp[[indexList_i]]
         
       } else if (sum(z) != length(colnames(newHapList_tmp[[indexList_i]])) & sum(z) != length(colnames(newHapList_tmp[[indexList_j]]))) { #hap is partially contained in hap+1
         
         if (sum(z) == 1) { #hap and hap+1 share one single variant
-          
+          #print("option D")
           newHapList_tmp[[indexList_i]] <- merge(newHapList_tmp[[indexList_i]], subset(newHapList_tmp[[indexList_j]], by=which(z)))
           
         } else if (sum(z) > 1) { #hap and hap+1 share >1 variant
-          
+          #print("option E")
           index_tmp <- 1:length(which(z))
           newHapList_tmp[[indexList_i]] <- merge(newHapList_tmp[[indexList_i]][,-c(which(z)[-c(length(which(z)))])], newHapList_tmp[[indexList_j]], by=colnames(newHapList_tmp[[indexList_i]])[which(z)[length(index_tmp)]])
           
@@ -450,93 +453,160 @@ subsettingByQuality_computingLQLinks <- function(outFName) {
 ##---------------------
 #--------------
 
+#######################
+## NEW FUNCTIONS - july 17 2017
+##
+#######################
 #this function takes the subMatrix following QC and generates the possible haplotypes between a provided LQ variant and all the possible
 #and previously built HQ haplotypes. It exports a list with the haplotype
-#it takes global variables: hapList, subMatrix, 
+#it takes global variables: hapList, subMatrix, LQhapQC
 creatingLQhaplotypes <- function(LQVarCoord) {
   
   #pos <- 3
-  #LQVarCoord <- "23762348"
-  #checks how many pairwise comb in the LQ matrix fall involve variants within HQ haplotypes 
+  #LQVarCoord <- "24152993"
+  #checks how many pairwise comb in the LQ matrix are related to HQ haplotypes and for how many sites within each HQ hap are covered by the LQ list
+  #overlappingHap is a vector of the length of the hapList and the numbers represent the nb of sites with links information in the LQ matrix
   overlappingHap <- unlist(lapply(1:length(hapList), function(x) { sum(is.element(subMatrix[which(subMatrix[,1] == LQVarCoord),2], colnames(hapList[[x]]))) }))
-  corrPC_one <- c()
-  corrPC_two <- c()
-  newHap <- list()  
-  #hapNb <- 75
-  countHapOver <- 1
-  for (hapNb in which(overlappingHap >= 2)) {
+  indexOverlappingHap <-  which(overlappingHap >= 2) #note before was >=
+  print(length(indexOverlappingHap))
+  
+  if (length(indexOverlappingHap) > 0) {
     
-    perHQhap_NewLQPos_m <- subMatrix[intersect(which(subMatrix[,1] == LQVarCoord),which(subMatrix[,2] %in% as.numeric(colnames(hapList[[hapNb]])))),]
-    perHQhap_NewLQPos_order_m <- subOrder[intersect(which(subMatrix[,1] == LQVarCoord),which(subMatrix[,2] %in% as.numeric(colnames(hapList[[hapNb]])))),]
-    vectorList <- list()
-    
-    
-    for (col in 1:nrow(perHQhap_NewLQPos_m)) {
+    corrPC_one <- c()
+    corrPC_two <- c()
+    newHap <- list()  
+    #hapNb <- 75
+    countHapOver <- 1
+    for (hapNb in indexOverlappingHap) {
       
-      linkSupported <- perHQhap_NewLQPos_order_m[col,][1:2]
+      #subsetting the subMatrix per LQ variant
+      perHQhap_NewLQPos_m <- subMatrix[intersect(which(subMatrix[,1] == LQVarCoord),which(subMatrix[,2] %in% as.numeric(colnames(hapList[[hapNb]])))),]
+      #subsetting the matrix with the links' order
+      perHQhap_NewLQPos_order_m <- subOrder[intersect(which(subMatrix[,1] == LQVarCoord),which(subMatrix[,2] %in% as.numeric(colnames(hapList[[hapNb]])))),]
+      vectorList <- list()
       
       
-      
-      if (is.element(1, linkSupported) & is.element(4, linkSupported)) {
+      for (col in 1:nrow(perHQhap_NewLQPos_m)) {
         
-        vectorList[[col]] <- rbind(c(0,0),c(1,1))
-        colnames(vectorList[[col]]) <- c(perHQhap_NewLQPos_m[col,1], perHQhap_NewLQPos_m[col,2])
+        linkSupported <- perHQhap_NewLQPos_order_m[col,][1:2]
         
-      } else if (is.element(2, linkSupported) & is.element(3, linkSupported)) {
         
-        vectorList[[col]] <- rbind(c(0,1),c(1,0))
-        colnames(vectorList[[col]]) <- c(perHQhap_NewLQPos_m[col,1], perHQhap_NewLQPos_m[col,2])
+        
+        if (is.element(1, linkSupported) & is.element(4, linkSupported)) {
+          
+          vectorList[[col]] <- rbind(c(0,0),c(1,1))
+          colnames(vectorList[[col]]) <- c(perHQhap_NewLQPos_m[col,1], perHQhap_NewLQPos_m[col,2])
+          
+        } else if (is.element(2, linkSupported) & is.element(3, linkSupported)) {
+          
+          vectorList[[col]] <- rbind(c(0,1),c(1,0))
+          colnames(vectorList[[col]]) <- c(perHQhap_NewLQPos_m[col,1], perHQhap_NewLQPos_m[col,2])
+          
+        }
+        
+        if (col == 1) {
+          
+          newHap[[countHapOver]] <- vectorList[[col]]
+          
+        } else {
+          
+          newHap[[countHapOver]] <- merge(newHap[[countHapOver]], vectorList[[col]], by = LQVarCoord)
+          
+          
+        }
         
       }
       
-      if (col == 1) {
+      # the first which does this - looking for the row number with a zero in the first overlapping site between the HQ hap and the new LQ hap that is not the LQ SITE
+      #the following vector contain the combination of states followed by the first zero in the HQ haplotype
+      hapZero_One <- as.vector(hapList[[hapNb]][which(hapList[[hapNb]][,colnames(hapList[[hapNb]])[colnames(hapList[[hapNb]]) %in% colnames(newHap[[countHapOver]])][1]] == 0 ),colnames(hapList[[hapNb]]) %in% colnames(newHap[[countHapOver]])])
+      #the following vector contain the combination of states followed by the first zero in the LQ haplotype
+      hapZero_Two <- as.vector(newHap[[countHapOver]][which(newHap[[countHapOver]][,colnames(newHap[[countHapOver]])[colnames(newHap[[countHapOver]]) %in% colnames(hapList[[hapNb]])][1]] == 0 ),colnames(newHap[[countHapOver]]) %in% colnames(hapList[[hapNb]])])
+      #example:
+      #HQ hap 
+      # SNP1 SNP2
+      # 0    1
+      # 1    0
+      #SNP1 and 2 are overlapping between the HQ hap and the new LQ hap
+      # hapZero_One contains coordinate c(0, column index(SNP1, SNP2))
+      
+      #the two vectors below contain the same as before but the combination starting with 1.
+      hapOne_One <- as.vector(hapList[[hapNb]][which(hapList[[hapNb]][,colnames(hapList[[hapNb]])[colnames(hapList[[hapNb]]) %in% colnames(newHap[[countHapOver]])][1]] == 1 ),colnames(hapList[[hapNb]]) %in% colnames(newHap[[countHapOver]])])
+      hapOne_Two <- as.vector(newHap[[countHapOver]][which(newHap[[countHapOver]][,colnames(newHap[[countHapOver]])[colnames(newHap[[countHapOver]]) %in% colnames(hapList[[hapNb]])][1]] == 1 ),colnames(newHap[[countHapOver]]) %in% colnames(hapList[[hapNb]])])
+      
+      #here we compare the HQ hap with the LQ hap for the overlapping sites
+      corrPC_one[countHapOver] <- sum(apply(m <- rbind(hapZero_One, hapZero_Two), 2, function(x) { is.element(x[1], x[2])}))
+      corrPC_two[countHapOver] <- sum(apply(m <- rbind(hapOne_One, hapOne_Two), 2, function(x) { is.element(x[1], x[2])}))
+      countHapOver <- countHapOver+1
+      
+    } #close for across compatible HQ haps
+    
+    #corrPC_one/two contain the nb of EQUAL comparisons between the new and the HQ haplotype
+    #if the nb of comparisons is >= 5 the LQ is kept and the LQ haploytpe built
+    if (LQhapQC == "loose" & (sum(overlappingHap[overlappingHap >= 2]) >= 5)) {
+      if ((sum(corrPC_one)/sum(overlappingHap[overlappingHap >= 2]) == 1) & (sum(corrPC_one) == sum(corrPC_two))) {
         
-        newHap[[countHapOver]] <- vectorList[[col]]
+        finalHapMerged_Ordered <- list()
+        for (hapNb in 1:length(indexOverlappingHap)) {
+          
+          #merging the full HQ with the lowHQ haplotype
+          if (length(setdiff(colnames(hapList[[indexOverlappingHap[hapNb]]]),colnames(newHap[[hapNb]]))) >= 1) {
+            
+            lastVarCommon <- intersect(colnames(hapList[[indexOverlappingHap[hapNb]]]),colnames(newHap[[hapNb]]))[1]
+            varOut <- intersect(colnames(hapList[[indexOverlappingHap[hapNb]]]),colnames(newHap[[hapNb]]))[-1]
+            index_varOut <- match(varOut, colnames(hapList[[indexOverlappingHap[hapNb]]]))
+            
+            finalHapMerged <- merge(newHap[[hapNb]], hapList[[indexOverlappingHap[hapNb]]][,-index_varOut], by = lastVarCommon)
+            finalHapMerged_Ordered[[hapNb]] <- finalHapMerged[,order(as.numeric(colnames(finalHapMerged)), decreasing = F)]
+            
+          } else if (length(setdiff(colnames(hapList[[indexOverlappingHap[hapNb]]]),colnames(newHap[[hapNb]]))) == 0) {
+            
+            finalHapMerged_Ordered[[hapNb]] <- newHap[[hapNb]]
+          }
+          #return(finalHapMerged_Ordered)
+        }
+        
+        
+        cat(paste("Variant:", LQVarCoord, "kept.", sep=" "), file=outputLQPhasing, sep="\n", append=T)
+        if (length(finalHapMerged_Ordered) == 1) {
+          
+          finalHap <- unique(finalHapMerged_Ordered)
+          
+        } else if (length(finalHapMerged_Ordered) > 1) {
+          
+          finalHap <- excludingDuplicates(unique(finalHapMerged_Ordered))[[1]]
+          
+        }
+        
+        return(finalHap)
         
       } else {
         
-        newHap[[countHapOver]] <- merge(newHap[[countHapOver]], vectorList[[col]], by = LQVarCoord)
-        
-        
+        cat(paste("Variant:", LQVarCoord, "not supported or ERROR: haplotypes differ.", sep=" "), file=outputLQPhasing, sep="\n", append=T)
+        finalHap <- 0
+        return(finalHap)
       }
+    } else if (LQhapQC == "restricted") {
       
-    }
-    
-    hapZero_One <- as.vector(hapList[[hapNb]][which(hapList[[hapNb]][,colnames(hapList[[hapNb]])[colnames(hapList[[hapNb]]) %in% colnames(newHap[[countHapOver]])][1]] == 0 ),colnames(hapList[[hapNb]]) %in% colnames(newHap[[countHapOver]])])
-    hapZero_Two <- as.vector(newHap[[countHapOver]][which(newHap[[countHapOver]][,colnames(newHap[[countHapOver]])[colnames(newHap[[countHapOver]]) %in% colnames(hapList[[hapNb]])][1]] == 0 ),colnames(newHap[[countHapOver]]) %in% colnames(hapList[[hapNb]])])
-    
-    
-    hapOne_One <- as.vector(hapList[[hapNb]][which(hapList[[hapNb]][,colnames(hapList[[hapNb]])[colnames(hapList[[hapNb]]) %in% colnames(newHap[[countHapOver]])][1]] == 1 ),colnames(hapList[[hapNb]]) %in% colnames(newHap[[countHapOver]])])
-    hapOne_Two <- as.vector(newHap[[countHapOver]][which(newHap[[countHapOver]][,colnames(newHap[[countHapOver]])[colnames(newHap[[countHapOver]]) %in% colnames(hapList[[hapNb]])][1]] == 1 ),colnames(newHap[[countHapOver]]) %in% colnames(hapList[[hapNb]])])
-    
-    corrPC_one[countHapOver] <- sum(apply(m <- rbind(hapZero_One, hapZero_Two), 2, function(x) { is.element(x[1], x[2])}))
-    corrPC_two[countHapOver] <- sum(apply(m <- rbind(hapOne_One, hapOne_Two), 2, function(x) { is.element(x[1], x[2])}))
-    countHapOver <- countHapOver+1
-    
-  }
-  #corrPC_one/two contain the nb of EQUAL comparisons between the new and the HQ haplotype
-  #if the nb of comparisons is >= 5 the LQ is kept and the LQ haploytpe built
-  if ((sum(corrPC_one) >= 5  & sum(corrPC_two) >= 5) & (sum(corrPC_one) == sum(corrPC_two))) {
-    
-    cat(paste("Variant:", LQVarCoord, "kept.", sep=" "), file=outputLQPhasing, sep="\n", append=T)
-    if (length(newHap) == 1) {
-      
-      finalHap <- unique(newHap)
-    
-      } else if (length(newHap) > 1) {
-      
-      finalHap <- excludingDuplicates(unique(newHap))[[1]]
-      
-    }
+      cat(paste("Variant:", LQVarCoord, "not supported or ERROR: less than five comparisons.", sep=" "), file=outputLQPhasing, sep="\n", append=T)
+      finalHap <- 0
       return(finalHap)
+    } else {
+      
+      cat(paste("Variant:", LQVarCoord, "not supported or ERROR: haplotypes differ.", sep=" "), file=outputLQPhasing, sep="\n", append=T)
+      finalHap <- 0
+      return(finalHap)
+      
+    }
     
   } else {
     
-    cat(paste("Variant:", LQVarCoord, "not supported or ERROR: haplotypes differ.", sep=" "), file=outputLQPhasing, sep="\n", append=T)
+    cat(paste("Variant:", LQVarCoord, "not supported. ERROR: not enough HQ haplotypes", sep=" "), file=outputLQPhasing, sep="\n", append=T)
     finalHap <- 0
     return(finalHap)
+    
+    
   }
-  
   
 }
 
