8db9070d508089fb28563230955ea68dd5c600a4
Isabel Alves <ialves@ad.oicr.on.ca>
2017-08-13 15:39:28 -0400

    Added functions: assembleCompleteHap, LinkingLQvar_toHQhaps, buildingHQconnections

--- functions_Phasing.R
+++ functions_Phasing.R
@@ -852,7 +852,124 @@ taggingLQvar_HQhap_combination <- function(LQvar, list_full_HQhaps, nb_of_haplot
   return(hapCombination)
 }
 
+#######################
+##
+##
+#######################
 
+buildingHQ_connections <- function(hqListSize) { #takes hapList and hap_df as variables.
+  
+  
+  HQhap_index_x <- 1
+  HQhap_index_y <- HQhap_index_x+1
+  linksTags <- c("AD", "BC", "X")
+  tmpConnTag <- c()
+  
+  while (HQhap_index_x < hqListSize) {
+    
+    tmp_v <- as.vector(table(hap_df[which(hap_df[,2] == HQhap_index_x & hap_df[,3] == HQhap_index_y),5]))
+    
+    if (sum(tmp_v) > 5 & tmp_v[which(tmp_v == max(tmp_v))] >= 5*sum(tmp_v[-which(tmp_v == max(tmp_v))])) {
+      
+      
+      tmpConnTag <- c(tmpConnTag, paste(HQhap_index_x,  HQhap_index_y, linksTags[which(tmp_v == max(tmp_v))], sep = "_"))
+      HQhap_index_x <- HQhap_index_y
+      HQhap_index_y <- HQhap_index_x+1
+      
+    } else {
+      print("Error: not enough comparisons.")
+      HQhap_index_y <- HQhap_index_y+1
+    }
+    if (HQhap_index_y > hqListSize) {
+      break;
+    }
+  }
+  return(tmpConnTag)
+}
+
+
+
+##########################
+#####
+#### Establishes the connection between each LQ variants and the closest HQ haplotype
+#### Generates a list of the hapList length with the anchored LQ variants
+#####
+##########################
+
+linkingLQvar_toHQhaplotypes <- function() { #takes two GLOBAL variables: hapList, hap_df
+  
+  tmp_listHQhap_LQvar <- list()
+  length(tmp_listHQhap_LQvar) <- length(hapList)
+  sitesLQ <- as.numeric(as.vector(unique(hap_df[,1])))
+  
+  for (siteNb in 1:length(sitesLQ)) {
+    
+    #siteNb <- 5356
+    tmpHQIndex <- unique(as.numeric(as.vector(unlist(hap_df[which(hap_df[,1] == sitesLQ[siteNb]),2:3]))))
+    
+    if (sum(is.na(tmpHQIndex)) > 0) { #the LQ linked to only one HQ hap
+      
+      tmpHapNb <- tmpHQIndex[1]
+      
+    } else { #the LQ linked to >1 HQ hap
+      closerHaps <- order(sapply(tmpHQIndex, function(x) { abs(sitesLQ[siteNb]-as.numeric(colnames(hapList[[x]])[1])) } ))
+      tmpHapNb <- tmpHQIndex[closerHaps[1]]
+      
+    }
+    
+    
+    if (length(hap_df[which(hap_df[,1] == sitesLQ[siteNb] & hap_df[,2] == tmpHapNb),4]) != 0) { #the closest HQ hap is in the 2nd col
+      
+      tmpLinkTag <- hap_df[which(hap_df[,1] == sitesLQ[siteNb] & hap_df[,2] == tmpHapNb),4]
+      
+    } else if (length(hap_df[which(hap_df[,1] == sitesLQ[siteNb] & hap_df[,3] == tmpHapNb),4]) != 0) { #the closest HQ hap is in the 3rd col
+      
+      tmpLinkTagDouble <- hap_df[which(hap_df[,1] == sitesLQ[siteNb] & hap_df[,3] == tmpHapNb),4:5]
+      if (as.vector(unlist(tmpLinkTagDouble[1])) ==  as.vector(unlist(tmpLinkTagDouble[2]))) {
+        tmpLinkTag <- "AD"
+      } else {
+        tmpLinkTag <- "BC"
+      }
+      
+      
+    } else {
+      
+      print("Error: closest haplotype not found")
+      
+    }
+    
+    if (length(tmp_listHQhap_LQvar[[tmpHapNb]]) == 0) {
+      tmp_listHQhap_LQvar[[tmpHapNb]] <- paste(sitesLQ[siteNb], tmpLinkTag, sep="_")
+    } else {
+      tmp_listHQhap_LQvar[[tmpHapNb]] <- c(unlist(tmp_listHQhap_LQvar[[tmpHapNb]]),paste(sitesLQ[siteNb], tmpLinkTag, sep="_"))
+    }
+    
+  }
+  return(tmp_listHQhap_LQvar)
+}
+####-------------------
+##-------------
+
+
+#function to assemble haplotypes
+assemblingCompleteHaplotypes <- function(hapNb) {
+  
+  #hapNb <- 1
+  perHQhapLQvar <- data.frame(matrix(unlist(strsplit(listHQhap_LQvar[[hapNb]], split="_")), ncol=2, byrow = T))
+  tmpFullHapNames <- c(colnames(hapList[[hapNb]])[1], as.vector(perHQhapLQvar[,1]))
+  tmpZeroOneComb <- rep(0,length(as.vector(perHQhapLQvar[,2])))
+  tmpZeroOneComb[which(as.vector(perHQhapLQvar[,2]) == "BC")] <- 1
+  tmpFullHaplotype <- c(0, tmpZeroOneComb)
+  names(tmpFullHaplotype) <- tmpFullHapNames
+  
+  tmpFullHaplotype <- c(tmpFullHaplotype,hapList[[hapNb]][which(hapList[[hapNb]][,1] == 0),2:ncol(hapList[[hapNb]])])
+  
+  tmpOppositeHap <- rep(1, length(tmpFullHaplotype))
+  tmpOppositeHap[which(tmpFullHaplotype == 1)] <- 0
+  names(tmpOppositeHap) <- names(tmpFullHaplotype)
+  return(list(tmpFullHaplotype, tmpOppositeHap))
+  
+}
 
 #################################
 ##
